608bee74cfdead6a79282ecf1607063e78c1b3c3
adilarshidin <adil.therunningman@gmail.com>
2026-03-01 16:04:31 +0100

    feat: 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const payload = req.body;
+  await Todo.findByIdAndUpdate(req.todo._id, payload, { useFindAndModify: false })
+  res.sendStatus(200);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
